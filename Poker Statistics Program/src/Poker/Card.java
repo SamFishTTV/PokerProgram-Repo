0b90cc8e8f74b914c20afe6772e26045086430c4
sampi <sampi@10.0.0.172>
2022-07-18 14:33:32 -0400

    initial commit, created the Poker program as well as added the card class and card constructor method.

---
+++ Poker Statistics Program/src/Poker/Card.java
@@ -0,0 +1,23 @@
+package Poker;
+
+public class Card {
+
+    private String suit;
+    private String value;
+    
+    /*
+     * the Card class constructor, created a single card object
+     * @param suit is the suit of the card object being created as a string
+     * @param value is the value of the card object being created as a string
+     */
+    public Card(String suit, String value) {
+        this.suit = suit;
+        this.value = value;
+    }
+    
+    
+    
+    
+    
+    
+}
